b03bb3bb79a4143ebdbe1b576176d013cf19f3ec
Kyle Smith <kas.smith2000@gmail.com>
2021-02-12 18:35:32 -0500

    postfix working

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -4,14 +4,52 @@ defmodule Practice.Calc do
     num
   end
 
+  #Returns Pirority of operator
+  def op_priority(op) do
+    cond do
+      op == "+" or op == "-" -> 0
+      op == "*" or op == "/" -> 1
+      true -> -1
+    end
+  end
+
+  #Returns :op or :num
+  def op_or_num(str) do
+    if str == "+" or str == "-" or str == "*" or str == "/" do
+      :op
+    else
+      :num
+    end
+  end
+
+  def toPostfix(keyList, stack, postfix) do
+    if keyList == [] do
+      postfix ++ stack
+    else
+      curKey = elem(hd(keyList), 0)
+      curVal = elem(hd(keyList), 1)
+      if curKey == :num do #number
+        toPostfix(tl(keyList), stack, postfix ++ [curVal])
+      else #operator
+        cond do
+          stack != [] and op_priority(curVal) <= op_priority(hd(stack)) -> toPostfix(keyList, tl(stack), postfix ++ [hd(stack)])
+          true -> toPostfix(tl(keyList), [curVal | stack], postfix)
+        end
+      end
+    end
+  end
+
   def calc(expr) do
     # This should handle +,-,*,/ with order of operations,
     # but doesn't need to handle parens.
     expr
     |> String.split(~r/\s+/)
-    |> hd
-    |> parse_float
-    |> :math.sqrt()
+    |> Enum.map(fn(x) -> {op_or_num(x), x} end)
+    |> toPostfix([], [])
+
+    #|> hd
+    #|> parse_float
+    #|> :math.sqrt()
 
     # Hint:
     # expr
